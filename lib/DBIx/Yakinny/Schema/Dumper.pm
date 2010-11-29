0438dfd5b015d2935402bea5b14d5119767a9e26
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-11-29 15:56:25 +0900

    implemented schema::dumper

---
+++ lib/DBIx/Yakinny/Schema/Dumper.pm
@@ -0,0 +1,41 @@
+package DBIx::Yakinny::Schema::Dumper;
+use strict;
+use warnings;
+use utf8;
+use DBIx::Inspector;
+use Carp ();
+
+sub dump {
+    my $class = shift;
+    my %args = @_==1 ? %{$_[0]} : @_;
+
+    my $dbh = $args{dbh} or Carp::croak("missing mandatory parameter 'dbh'");
+    my $schema_class = $args{schema_class} or Carp::croak "missing mandatory parameter 'schema_class'";
+    my $inspector = DBIx::Inspector->new(dbh => $dbh);
+    my $ret = "package $schema_class;\n";
+    $ret .= "use strict;\n";
+    $ret .= "use warnings;\n";
+    $ret .= "use utf8;\n";
+    $ret .= "use parent qw(DBIx::Yakinny::Schema);\n\n";
+    for my $table ($inspector->tables) {
+        $ret .= "__PACKAGE__->register_table(\n";
+        $ret .= sprintf("  table => q{%s},\n", $table->name);
+        $ret .= sprintf("  columns => [qw(%s)],\n", join(' ', map { $_->name } $table->columns));
+        $ret .= sprintf("  pk      => [qw(%s)],\n", join(' ', map { $_->name } $table->primary_key));
+        $ret .= ");\n\n";
+    }
+    $ret .= "\n1;\n";
+    return $ret;
+}
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    use DBI;
+    use DBIx::Yakinny::Schema::Dumper;
+
+    my $dbh = DBI->connect(...) or die;
+    print DBIx::Yakinny::Schema::Dumper->dump(dbh => $dbh, schema_class => 'DBIx::Yakinny::Schema');
+
---
+++ t/05_schema_dumper.t
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::Requires 'DBD::SQLite';
+use DBI;
+
+# initialize
+my $dbh = DBI->connect('dbi:SQLite:', '', '', {RaiseError => 1}) or die 'cannot connect to db';
+$dbh->do(q{
+    create table user (
+        user_id integer primary key,
+        name varchar(255),
+        email varchar(255),
+        created_on int
+    );
+});
+
+{
+    package MyApp::DB;
+    use base qw/DBIx::Yakinny/;
+    use DBIx::Yakinny::Schema::Dumper;
+    my $code = DBIx::Yakinny::Schema::Dumper->dump(
+        dbh          => $dbh,
+        schema_class => 'MyApp::DB::Schema',
+    );
+    eval $code;
+    ::ok !$@, 'no syntax error';
+
+    __PACKAGE__->set_schema_class('MyApp::DB::Schema');
+}
+
+my $db = MyApp::DB->new(dbh => $dbh);
+my $user = $db->schema_class->get_class_for('user');
+is($user->table, 'user');
+is(join(',', @{$user->pk}), 'user_id');
+is(join(',', $user->columns), 'user_id,name,email,created_on');
+
+done_testing;
+
