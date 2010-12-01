34dea606c33e632ff45f902f1becb910e560f238
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-12-02 07:28:36 +0900

    Dumper dumps meta data

--- lib/DBIx/Yakinny/Schema/Dumper.pm
+++ lib/DBIx/Yakinny/Schema/Dumper.pm
@@ -2,8 +2,9 @@ package DBIx::Yakinny::Schema::Dumper;
 use strict;
 use warnings;
 use utf8;
-use DBIx::Inspector;
+use DBIx::Inspector 0.03;
 use Carp ();
+use Data::Dumper ();
 
 sub dump {
     my $class = shift;
@@ -14,10 +15,18 @@ sub dump {
     my $ret = "do {\n";
     $ret .= "use DBIx::Yakinny::Schema;\n";
     $ret .= "my \$schema = DBIx::Yakinny::Schema->new();\n";
+    local $Data::Dumper::Terse    = 1;
+    local $Data::Dumper::Indent   = 0;
+    local $Data::Dumper::Sortkeys = 1;
     for my $table ($inspector->tables) {
         $ret .= "\$schema->register_table(\n";
         $ret .= sprintf("  table => q{%s},\n", $table->name);
-        $ret .= sprintf("  columns => [qw(%s)],\n", join(' ', map { $_->name } $table->columns));
+        $ret .= sprintf("  columns => [\n");
+        for my $column ($table->columns) {
+        my $src = +{ map { $_ => $column->{$_}} qw/COLUMN_NAME DECIMAL_DIGITS COLUMN_DEF NUM_PREC_RADIX CHAR_OCTET_LENGTH REMARKS IS_NULLABLE COLUMN_SIZE ORDINAL_POSITION TYPE_NAME NULLABLE DATA_TYPE SQL_DATA_TYPE SQL_DATETIME_SUB/ };
+        $ret .= sprintf("    %s,\n", Data::Dumper::Dumper($src));
+        }
+        $ret .= sprintf("],\n");
         $ret .= sprintf("  primary_key      => [qw(%s)],\n", join(' ', map { $_->name } $table->primary_key));
         $ret .= ");\n\n";
     }
