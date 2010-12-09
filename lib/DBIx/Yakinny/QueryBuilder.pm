e9116a0a08bb77467696ea106e6944574fc76d57
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-12-09 09:08:55 +0900

    SQL::Builder renamed to SQL::Maker.

--- lib/DBIx/Yakinny/QueryBuilder.pm
+++ lib/DBIx/Yakinny/QueryBuilder.pm
@@ -2,7 +2,7 @@ package DBIx::Yakinny::QueryBuilder;
 use strict;
 use warnings;
 use utf8;
-use parent qw/SQL::Builder/;
+use parent qw/SQL::Maker/;
 
 __PACKAGE__->load_plugin($_)
     for qw/InsertMulti/;
