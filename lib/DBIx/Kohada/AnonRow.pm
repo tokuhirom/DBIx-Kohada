d6cc7cff6af8db7fce70e5fa09c189e3be587813
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-12-15 09:33:06 +0900

    - AnonRow->columns respects context - docs

--- lib/DBIx/Kohada/AnonRow.pm
+++ lib/DBIx/Kohada/AnonRow.pm
@@ -13,7 +13,8 @@ sub new {
 
 sub columns {
     my $self = shift;
-    keys %$self;
+    my @columns = keys %$self;
+    return wantarray ? @columns : \@columns;
 }
 
 our $AUTOLOAD;
@@ -37,3 +38,59 @@ sub get_columns {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+DBIx::Kohada::AnonRow - Anonymous Row Object
+
+=head1 DESCRIPTION
+
+This object is an anonymous row object. It's instance is created by C<$db->search_by_sql(undef, $sql, @binds)> or C<$db->search_by_query_object($query)> method.
+
+This class defines B<AUTOLOAD> method. If you calls missing method, then you get the column data on the method name.
+
+    $row->foo();
+    # is equivalent to
+    $row->get_column('foo');
+
+=head1 METHODS
+
+=over 4
+
+=item my $row = DBIx::Kohada::AnonRow->new(%args);
+
+Create new instance of DBIx::Kohada::AnonRow. The arguments are:
+
+=over 4
+
+=item \%row_data
+
+It contains fetched data in hashref.
+
+=back
+
+=item my @columns = $row->columns();
+
+=item my $columns = $row->columns(); # ArrayRef in scalar context.
+
+This method returns the fetched column names.
+
+This method looks context by wantarray.
+
+=item my $val = $row->get_column($column_name);
+
+Return: The value of column.
+
+Get the column data from row.
+
+This method throws exception if the $column_name is not exists in the fetched data.
+
+=item my $data = $row->get_columns();
+
+Returns all loaded column data as a hash, containing raw values. To get
+just one value for a particular column, use "get_column".
+
+Returns: A hashref of columnname, value pairs.
+
+=back
